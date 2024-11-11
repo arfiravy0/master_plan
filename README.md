2500c36086b50eb90b85f4525fda898f1f6528c3
arfiravy0 <arfiraandayani1000@gmail.com>
2024-11-11 14:57:19 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Nama: Arfira Andayani
+NIM: 362358302154
+Kelas: 2B
+
+PRAKTIKUM 1
+![1](image.png)
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:master_plan/view/plan_screen.dart';
+
+void main() => runApp(MasterPlanApp());
+
+class MasterPlanApp extends StatelessWidget {
+  const MasterPlanApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: PlanScreen(),
+    );
+  }
+}
